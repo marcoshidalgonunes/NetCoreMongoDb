b33f69e57474c5ac2f6a62742bcaf2114f9428cd
Marcos Hidalgo Nunes <marcoshidalgonunes@msn.com>
2022-01-28 13:26:15 -0300

    Study for Elastic APM support.

---
+++ src/back-end/Catalog.ElasticApm/Extensions/ExceptionMiddlewareExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace Catalog.Core
+{
+    public static class ExceptionMiddlewareExtensions
+    {
+        public static void UseExceptionMiddleware(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<ExceptionMiddleware>();
+        }
+    }
+}
