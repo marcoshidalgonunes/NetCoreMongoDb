9f6adcf6968e3c0b52bc81998caf9bd0ee36499f
Marcos Hidalgo Nunes <marcoshidalgonunes@msn.com>
2023-05-11 10:27:08 -0300

    Refactored to .NET 6.0 standards.

--- src/back-end/Catalog.Data.Test/Fixture/DependencyInjectionFixture.cs
+++ src/back-end/Catalog.Data.Test/Fixture/DependencyInjectionFixture.cs
@@ -8,36 +8,35 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 
-namespace Catalog.Data.Test.Fixture
+namespace Catalog.Data.Test.Fixture;
+
+public sealed class DependencyInjectionFixture : IDisposable
 {
-    public sealed class DependencyInjectionFixture : IDisposable
+    public ServiceProvider ServiceProvider { get; private set; }
+
+    public DependencyInjectionFixture()
     {
-        public ServiceProvider ServiceProvider { get; private set; }
+        EntityMongoMapper.Map<Book, string>();
 
-        public DependencyInjectionFixture()
-        {
-            EntityMongoMapper.Map<Book, string>();
+        var appsettings = "{ \"MongoDbSettings\": { \"CollectionName\": \"Books\", \"ConnectionString\": \"mongodb://localhost:27017\", \"DatabaseName\": \"BookstoreDb\" } }";
 
-            var appsettings = "{ \"MongoDbSettings\": { \"CollectionName\": \"Books\", \"ConnectionString\": \"mongodb://localhost:27017\", \"DatabaseName\": \"BookstoreDb\" } }";
+        var config = new ConfigurationBuilder()
+            .AddJsonStream(new MemoryStream(Encoding.ASCII.GetBytes(appsettings)))
+            .Build();
 
-            var config = new ConfigurationBuilder()
-                .AddJsonStream(new MemoryStream(Encoding.ASCII.GetBytes(appsettings)))
-                .Build();
+        var services = new ServiceCollection();
 
-            var services = new ServiceCollection();
+        services.Configure<MongoDbSettings>(
+            config.GetSection(nameof(MongoDbSettings)));
 
-            services.Configure<MongoDbSettings>(
-                config.GetSection(nameof(MongoDbSettings)));
+        services.AddSingleton(sp =>
+            sp.GetRequiredService<IOptions<MongoDbSettings>>().Value);
 
-            services.AddSingleton(sp =>
-                sp.GetRequiredService<IOptions<MongoDbSettings>>().Value);
+        ServiceProvider = services.BuildServiceProvider();
+    }
 
-            ServiceProvider = services.BuildServiceProvider();
-        }
-
-        public void Dispose()
-        {
-            ServiceProvider.Dispose();
-        }
+    public void Dispose()
+    {
+        ServiceProvider.Dispose();
     }
 }
--- src/back-end/Catalog.Service.Test/BookServiceTest.cs
+++ src/back-end/Catalog.Service.Test/BookServiceTest.cs
@@ -5,200 +5,199 @@ using Catalog.Domain.Entity;
 using Moq;
 using Xunit;
 
-namespace Catalog.Service.Test
+namespace Catalog.Service.Test;
+
+public sealed class BookServiceTest
 {
-    public sealed class BookServiceTest
+    private readonly Mock<IMongoDbRepository<Book, string>> repositoryMock = new Mock<IMongoDbRepository<Book, string>>();
+
+    [Fact]
+    public async Task Create()
     {
-        private readonly Mock<IMongoDbRepository<Book, string>> repositoryMock = new Mock<IMongoDbRepository<Book, string>>();
-
-        [Fact]
-        public async Task Create()
+        // Arrange
+        Book book = new()
         {
-            // Arrange
-            Book book = new()
+            Author = "Ralph Johnson",
+            Name = "Design Patterns",
+            Category = "Computers",
+            Price = 54.90M
+        };
+        repositoryMock
+            .Setup(o => o.CreateAsync(book))
+            .ReturnsAsync(new Book
             {
-                Author = "Ralph Johnson",
-                Name = "Design Patterns",
-                Category = "Computers",
-                Price = 54.90M
-            };
-            repositoryMock
-                .Setup(o => o.CreateAsync(book))
-                .ReturnsAsync(new Book
-                {
+                Id = "613260743633c438d5250513",
+                Author = book.Author,
+                Name = book.Name,
+                Category = book.Category,
+                Price = book.Price
+            });
+        var service = new BookService(repositoryMock.Object);
+
+        // Act
+        var item = await service.CreateAsync(book);
+
+        // Assert
+        Assert.True(item.Name == book.Name);
+    }
+
+    [Fact]
+    public async Task GetAll()
+    {
+        // Arrange
+        repositoryMock
+            .Setup(o => o.ReadAllAsync())
+            .ReturnsAsync(new List<Book> {
+                new Book {
                     Id = "613260743633c438d5250513",
-                    Author = book.Author,
-                    Name = book.Name,
-                    Category = book.Category,
-                    Price = book.Price
-                });
-            var service = new BookService(repositoryMock.Object);
-
-            // Act
-            var item = await service.CreateAsync(book);
-
-            // Assert
-            Assert.True(item.Name == book.Name);
-        }
-
-        [Fact]
-        public async Task GetAll()
-        {
-            // Arrange
-            repositoryMock
-                .Setup(o => o.ReadAllAsync())
-                .ReturnsAsync(new List<Book> {
-                    new Book {
-                        Id = "613260743633c438d5250513",
-                        Author = "Ralph Johnson",
-                        Name = "Design Patterns",
-                        Category = "Computers",
-                        Price = 54.90M
-                    },
-                    new Book {
-                        Id = "613260743633c438d5250514",
-                        Author = "Robert C. Martin",
-                        Name = "Clean Code",
-                        Category = "Computers",
-                        Price = 43.15M
-                    }
-                });
-            var service = new BookService(repositoryMock.Object);
-
-            // Act
-            var items = await service.GetAllAsync();
-
-            // Assert
-            Assert.True(items.Count > 0);
-        }
-
-        [Theory]
-        [InlineData("Name", "Design Patterns")]
-        [InlineData("Category", "Computers")]
-        public async Task GetByCriteria(string criteria, string search)
-        {
-            // Arrange
-            repositoryMock
-                .Setup(o => o.ReadByCriteriaAsync(criteria, search))
-                .ReturnsAsync(new List<Book> {
-                    new Book {
-                        Id = "613260743633c438d5250513",
-                        Author = "Ralph Johnson",
-                        Name = "Design Patterns",
-                        Category = "Computers",
-                        Price = 54.90M
-                    },
-                    new Book {
-                        Id = "613260743633c438d5250514",
-                        Author = "Robert C. Martin",
-                        Name = "Clean Code",
-                        Category = "Computers",
-                        Price = 43.15M
-                    }
-                });
-            var service = new BookService(repositoryMock.Object);
-
-            // Act
-            var items = await service.GetByCriteriaAsync(criteria, search);
-
-            // Assert
-            Assert.True(items.Count > 0);
-        }
-
-        [Theory]
-        [InlineData("Category", "Games")]
-        [InlineData("Style", "Computers")]
-        public async Task GetByCriteriaNotFound(string criteria, string search)
-        {
-            // Arrange
-            repositoryMock
-                .Setup(o => o.ReadByCriteriaAsync(criteria, search))
-                .ReturnsAsync(value: null);
-            var service = new BookService(repositoryMock.Object);
-
-            // Act
-            var items = await service.GetByCriteriaAsync(criteria, search);
-
-            // Assert
-            Assert.Null(items);
-        }
-
-        [Theory]
-        [InlineData("613260743633c438d5250513")]
-        public async Task GetById(string id)
-        {
-            // Arrange
-            repositoryMock
-                .Setup(o => o.ReadByIdAsync(id))
-                .ReturnsAsync(new Book
-                {
-                    Id = id,
                     Author = "Ralph Johnson",
                     Name = "Design Patterns",
                     Category = "Computers",
                     Price = 54.90M
-                });
-            var service = new BookService(repositoryMock.Object);
+                },
+                new Book {
+                    Id = "613260743633c438d5250514",
+                    Author = "Robert C. Martin",
+                    Name = "Clean Code",
+                    Category = "Computers",
+                    Price = 43.15M
+                }
+            });
+        var service = new BookService(repositoryMock.Object);
 
-            // Act
-            var item = await service.GetByIdAsync(id);
+        // Act
+        var items = await service.GetAllAsync();
 
-            // Assert
-            Assert.NotNull(item);
-        }
+        // Assert
+        Assert.True(items.Count > 0);
+    }
 
-        [Theory]
-        [InlineData("613260743633c438d5250513")]
-        public async Task GetByIdNotFound(string id)
-        {
-            // Arrange
-            repositoryMock
-                .Setup(o => o.ReadByIdAsync(id))
-                .ReturnsAsync(value: null);
-            var service = new BookService(repositoryMock.Object);
+    [Theory]
+    [InlineData("Name", "Design Patterns")]
+    [InlineData("Category", "Computers")]
+    public async Task GetByCriteria(string criteria, string search)
+    {
+        // Arrange
+        repositoryMock
+            .Setup(o => o.ReadByCriteriaAsync(criteria, search))
+            .ReturnsAsync(new List<Book> {
+                new Book {
+                    Id = "613260743633c438d5250513",
+                    Author = "Ralph Johnson",
+                    Name = "Design Patterns",
+                    Category = "Computers",
+                    Price = 54.90M
+                },
+                new Book {
+                    Id = "613260743633c438d5250514",
+                    Author = "Robert C. Martin",
+                    Name = "Clean Code",
+                    Category = "Computers",
+                    Price = 43.15M
+                }
+            });
+        var service = new BookService(repositoryMock.Object);
 
-            // Act
-            var item = await service.GetByIdAsync(id);
+        // Act
+        var items = await service.GetByCriteriaAsync(criteria, search);
 
-            // Assert
-            Assert.Null(item);
-        }
+        // Assert
+        Assert.True(items.Count > 0);
+    }
 
-        [Fact]
-        public async Task Update()
-        {
-            // Arrange
-            Book book = new Book
+    [Theory]
+    [InlineData("Category", "Games")]
+    [InlineData("Style", "Computers")]
+    public async Task GetByCriteriaNotFound(string criteria, string search)
+    {
+        // Arrange
+        repositoryMock
+            .Setup(o => o.ReadByCriteriaAsync(criteria, search))
+            .ReturnsAsync(value: null);
+        var service = new BookService(repositoryMock.Object);
+
+        // Act
+        var items = await service.GetByCriteriaAsync(criteria, search);
+
+        // Assert
+        Assert.Null(items);
+    }
+
+    [Theory]
+    [InlineData("613260743633c438d5250513")]
+    public async Task GetById(string id)
+    {
+        // Arrange
+        repositoryMock
+            .Setup(o => o.ReadByIdAsync(id))
+            .ReturnsAsync(new Book
             {
-                Id = "613260743633c438d5250513",
+                Id = id,
                 Author = "Ralph Johnson",
                 Name = "Design Patterns",
                 Category = "Computers",
                 Price = 54.90M
-            };
-            repositoryMock.Setup(o => o.UpdateAsync(book));
-            var service = new BookService(repositoryMock.Object);
+            });
+        var service = new BookService(repositoryMock.Object);
 
-            // Act
-            var exception = await Record.ExceptionAsync(() => service.UpdateAsync(book));
+        // Act
+        var item = await service.GetByIdAsync(id);
 
-            // Assert
-            Assert.Null(exception);
-        }
+        // Assert
+        Assert.NotNull(item);
+    }
 
-        [Theory]
-        [InlineData("613260743633c438d5250513")]
-        public async Task Delete(string id)
+    [Theory]
+    [InlineData("613260743633c438d5250513")]
+    public async Task GetByIdNotFound(string id)
+    {
+        // Arrange
+        repositoryMock
+            .Setup(o => o.ReadByIdAsync(id))
+            .ReturnsAsync(value: null);
+        var service = new BookService(repositoryMock.Object);
+
+        // Act
+        var item = await service.GetByIdAsync(id);
+
+        // Assert
+        Assert.Null(item);
+    }
+
+    [Fact]
+    public async Task Update()
+    {
+        // Arrange
+        Book book = new Book
         {
-            // Arrange
-            repositoryMock.Setup(o => o.DeleteAsync(id));
-            var service = new BookService(repositoryMock.Object);
+            Id = "613260743633c438d5250513",
+            Author = "Ralph Johnson",
+            Name = "Design Patterns",
+            Category = "Computers",
+            Price = 54.90M
+        };
+        repositoryMock.Setup(o => o.UpdateAsync(book));
+        var service = new BookService(repositoryMock.Object);
 
-            // Act
-            var exception = await Record.ExceptionAsync(() => service.DeleteAsync(id));
+        // Act
+        var exception = await Record.ExceptionAsync(() => service.UpdateAsync(book));
 
-            // Assert
-            Assert.Null(exception);
-        }
+        // Assert
+        Assert.Null(exception);
+    }
+
+    [Theory]
+    [InlineData("613260743633c438d5250513")]
+    public async Task Delete(string id)
+    {
+        // Arrange
+        repositoryMock.Setup(o => o.DeleteAsync(id));
+        var service = new BookService(repositoryMock.Object);
+
+        // Act
+        var exception = await Record.ExceptionAsync(() => service.DeleteAsync(id));
+
+        // Assert
+        Assert.Null(exception);
     }
 }
